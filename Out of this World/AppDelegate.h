5e818284820c46c46afa8bf440f98467f6b809c0
Neil Houselander <neilhouselander@sky.com>
2015-02-15 09:38:12 +0000

    Initial Commit

---
+++ Out of this World/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Out of this World
+//
+//  Created by Neil Houselander on 15/02/2015.
+//  Copyright (c) 2015 Neil Houselander. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Out of this World/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Out of this World
+//
+//  Created by Neil Houselander on 15/02/2015.
+//  Copyright (c) 2015 Neil Houselander. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
